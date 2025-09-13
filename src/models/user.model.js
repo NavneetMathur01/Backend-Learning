c1c76cb00c8e94eed1630047e2f9a5a1d8896d93
NavneetMathur01 <navneetmathur98@gmail.com>
2025-09-13 15:04:03 +0530

    register controller fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema({
     refreshToken:{
         type:String
     }
-},{timeseries:true})
+},{timestamps:true})
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,7 +19,8 @@ const uploadOnCloudinary = async (localFilePath)=>{
         })
 
         // file has been upload successfully
-        console.log("file has been uploaded on cloudinary",response.url);
+        // console.log("file has been uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
